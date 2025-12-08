d27400b20e9378c8d8db00cc955e48301e6c5e1f
Hussien Mahmoud <hussien1498mahmoud@gmail.com>
2025-12-08 03:59:33 +0200

    remove unused images

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Happiness from "../../Components/Home/Happiness/Happiness"
 import HowApply from "../../Components/Home/HowApply/HowApply"
 import CommonService from "../../Components/Services/Common-Service/CommonService"
 // import ServicesGroup from "../../Components/Services/ServicesGroup/ServicesGroup"
-import Test from "../../Components/Test/Test"
+// import Test from "../../Components/Test/Test"
 import HomeBanner from "../../Components/Home/HomeBanner/HomeBanner"
 import AboutWhyUs from "../../Components/AboutUs/AboutWhyUs"
 
